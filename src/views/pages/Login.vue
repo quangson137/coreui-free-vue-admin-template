f59d2972f8335063a01e9adfbcd5c635130c8e6c
sonpq <quangson137@gmail.com>
2022-09-17 09:27:04 +0700

    update

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -2,64 +2,26 @@
   <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
     <CContainer>
       <CRow class="justify-content-center">
-        <CCol :md="8">
+        <CCol :md="7">
           <CCardGroup>
-            <CCard class="p-4">
-              <CCardBody>
-                <CForm>
-                  <h1>Login</h1>
-                  <p class="text-medium-emphasis">Sign In to your account</p>
-                  <CInputGroup class="mb-3">
-                    <CInputGroupText>
-                      <CIcon icon="cil-user" />
-                    </CInputGroupText>
-                    <CFormInput
-                      placeholder="Username"
-                      autocomplete="username"
-                    />
-                  </CInputGroup>
-                  <CInputGroup class="mb-4">
-                    <CInputGroupText>
-                      <CIcon icon="cil-lock-locked" />
-                    </CInputGroupText>
-                    <CFormInput
-                      type="password"
-                      placeholder="Password"
-                      autocomplete="current-password"
-                    />
-                  </CInputGroup>
-                  <CRow>
-                    <CCol :xs="3">
-                      <CButton color="primary" class="px-4"> Login </CButton>
-                    </CCol>
-                    <CCol :xs="5" class="text-right">
-                      <CButton color="link" class="px-0">
-                        Forgot password?
-                      </CButton>
-                    </CCol>
-                    <CCol :xs="3">
-                      <GoogleLogin :callback="googleLoginCallback">
-                        <CButton color="secondary" class="px-4">
-                          Google
-                        </CButton>
-                      </GoogleLogin>
-                    </CCol>
-                  </CRow>
-                </CForm>
-              </CCardBody>
-            </CCard>
-            <CCard class="text-white bg-primary py-5" style="width: 44%">
+            <CCard
+              class="text-success info py-5"
+              variant="'outline"
+              style="width: 44%"
+            >
               <CCardBody class="text-center">
                 <div>
-                  <h2>Sign up</h2>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                  <CButton color="light" variant="outline" class="mt-3">
-                    Register Now!
-                  </CButton>
+                  <h1>Citigo Merchant Account</h1>
+                  <GoogleLogin :callback="googleLoginCallback">
+                    <CButton
+                      color="info"
+                      variant="outline"
+                      size="lg"
+                      class="px-8"
+                    >
+                      Login with your Citigo Account
+                    </CButton>
+                  </GoogleLogin>
                 </div>
               </CCardBody>
             </CCard>
@@ -80,18 +42,5 @@ export default {
       this.$router.push('/')
     },
   },
-  // methods: {
-  //   async login() {
-  //     googleOneTap()
-  //       .then((response) => {
-  //         console.log('Handle response', response)
-  //         localStorage.setItem('jwt', 'Ahihi')
-  //         this.$router.push('/')
-  //       })
-  //       .catch((e) => {
-  //         console.log('Error', e)
-  //       })
-  //   },
-  // },
 }
 </script>
